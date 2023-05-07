b43464f383ece2a9e3d5674766ea141402314cd9
ChrisKnowsDev <chrisharley81@gmail.com>
2023-05-07 07:28:16 -0400

    added small sale text change from json data

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ let jsonData;
 const price = document.querySelector(".price");
 const subscribeLink = document.querySelector(".content-footer-span");
 const subscribeText = document.querySelector(".subscribe-text");
+const saveSmallText = document.querySelector(".save-small-text");
+
+// set subscribe text
+if (localStorage.getItem("value")) {
+  subscribeText.textContent = localStorage.getItem("value");
+}
 
 // get local json data
 async function getData(url) {
@@ -11,8 +17,10 @@ async function getData(url) {
 
   if (data.isOfferActive) {
     price.textContent = "$139";
+    saveSmallText.textContent = "$139";
   } else {
     price.textContent = "$119";
+    saveSmallText.textContent = "$119";
   }
 }
 
@@ -20,5 +28,8 @@ getData("./data/index.json");
 
 // handle subscribe click
 subscribeLink.addEventListener("click", () => {
-  subscribeText.textContent = "Thank you for subscribing!";
+  const thanks = "Thank you for subscribing!";
+  localStorage.setItem("value", thanks);
+  console.log(localStorage.getItem("value from localstorage", "value"));
+  subscribeText.textContent = localStorage.getItem("value");
 });
